9478636b07368c89ec4d06211ef24edbfeca8ebb
dirkdesmet <15632862+dirkdesmet@users.noreply.github.com>
2019-09-12 14:06:18 +0200

    Create copy-jira-ticket-title.js
    Added old version to github

---
+++ copy-jira-ticket-title.js
@@ -0,0 +1,25 @@
+(function() {
+    /* copy selected issues from agile planning lists */
+    var issues = jQuery('.ghx-selected').filter('.js-issue').map(function(i, issue) {
+        var k = jQuery(issue).attr("data-issue-key");
+        var s = jQuery(issue).find('.ghx-summary').attr("title");
+        return {
+            key: k,
+            summary: s
+        }
+    }).toArray();
+    /* copy issue from open issue */
+    var t = document.title.replace(" - JIRA@XploreGroup", "");
+    var match = t.match(/\[(.*)\]/);
+    if (match) {
+        var k = match[1];
+        issues.push({
+            key: k,
+            summary: t.replace("[" + k + "] ", "")
+        })
+    } /* create list */
+    var links = jQuery.map(issues, function(i) {
+        return "[" + i.key + "] " + i.summary;
+    });
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", links.join("\r\n"))
+})();
