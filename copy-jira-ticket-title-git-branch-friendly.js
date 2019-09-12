8c58b61a9e67d552f4808698e0db3acd2687ae06
dirkdesmet <15632862+dirkdesmet@users.noreply.github.com>
2019-09-12 14:31:05 +0200

    Create copy-jira-ticket-title-git-branch-friendly.js
    Added old version to github

---
+++ copy-jira-ticket-title-git-branch-friendly.js
@@ -0,0 +1,13 @@
+(function() {
+    /* copy selected issues from agile planning lists */
+    var issues = jQuery('.ghx-selected').filter('.js-issue').map(function(i, issue) {
+        var k = jQuery(issue).attr("data-issue-key");
+        var s = jQuery(issue).find('.ghx-summary').attr("title");
+        return {
+            key: k,
+            summary: s
+        }
+    }).toArray(); /* copy issue from open issue */
+    var t = document.title.replace(/(\[|\]|\:|"|'| - JIRA@.*$)/g, "").replace(/ |\//g, "-");
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", t)
+})()
