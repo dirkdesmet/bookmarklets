946f56315ccec2e4e6fa51f19b07ee06aa2f194e
Dirk De Smet <dirk.desmet@elision.eu>
2025-04-19 10:28:48 +0200

    Added bookmarklet to pass links to archive.is

---
+++ open-link-in-archive.is.js
@@ -0,0 +1,12 @@
+(() => {
+  const currentUrl = document.location.href;
+  const uselessParams = ['journeybuilder','utm_source'];
+  const url = new URL(currentUrl);
+  const params = new URLSearchParams(url.search);
+  const desktopDomain = url.host.replace(/^m\./, 'www.');
+  const filteredParams = new URLSearchParams([...url.searchParams.entries()].filter(([key, value]) => !uselessParams.includes(key)));
+  
+  url.host = desktopDomain;
+  url.search = filteredParams.toString();
+  document.location.href=`https://archive.is/${url.toString()}`;
+});
